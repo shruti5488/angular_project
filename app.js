3bb21ac4449f6e023201e772da083fa5334c699c
Shruti Priya <shruti5488@gmail.com>
2016-10-26 11:59:09 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Shruti is pro");
+// $(document).ready(function(){
+// 	debugger;
+// 	$("#menu-toggle").click(function(e) {
+//         e.preventDefault();
+//         $("#sidebar").toggleClass("active");
+// 	});
+// }
+
+//  	var app = angular.module('myApp', []);
+// 	app.controller('mainController', function($scope){
+// 		debugger;
+// 		$scope.legs = ["Milk", "Bread", "Cheese"];
+// 		// $scope.ViewLeg = function() {	
+// 		// 	debugger; 		
+// 		// 	$http({
+// 		// 		method: 'GET',
+//   //  				url: 'php.php'
+// 		// 	}).then(function (response) {
+//   //  			// code to execute in case of success
+// 		// 	}, function (response) {
+//    			// code to execute in case of error
+// 		});
+
+
