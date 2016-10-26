fd9083959446954f05101a52e372d43881835023
Tarun Arora <tananrules@gmail.com>
2016-10-27 01:21:45 +0530

    angular integration working

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-console.log("Shruti is pro");
+// console.log("Shruti is pro");
 // $(document).ready(function(){
-// 	debugger;
-// 	$("#menu-toggle").click(function(e) {
+//  debugger;
+//  $("#menu-toggle").click(function(e) {
 //         e.preventDefault();
 //         $("#sidebar").toggleClass("active");
-// 	});
+//  });
 // }
 
-//  	var app = angular.module('myApp', []);
-// 	app.controller('mainController', function($scope){
-// 		debugger;
-// 		$scope.legs = ["Milk", "Bread", "Cheese"];
-// 		// $scope.ViewLeg = function() {	
-// 		// 	debugger; 		
-// 		// 	$http({
-// 		// 		method: 'GET',
-//   //  				url: 'php.php'
-// 		// 	}).then(function (response) {
-//   //  			// code to execute in case of success
-// 		// 	}, function (response) {
-//    			// code to execute in case of error
-// 		});
+var app = angular.module('myApp', [])
+.controller('mainController', function($scope){
+  debugger;
+  $scope.legs = ["Milk", "Bread", "Cheese"];
+  $scope.ViewLeg = function() { 
+    debugger;     
+    $http({
+      method: 'GET',
+      url: 'php.php'
+    }).then(function (response) {
+      // code to execute in case of success
+    }, function (response) {
+      // code to execute in case of error
+    })
+  }
+});
 
 
