dc0cf3a94213868773799b42677808e30b8be333
Shruti Priya <shruti5488@gmail.com>
2016-11-05 16:29:00 -0700

    Pagination fixed all tabs

--- logic.php
+++ logic.php
@@ -18,6 +18,12 @@ else{
 
 	$start_url_leg = "http://congress.api.sunlightfoundation.com/legislators?apikey=dc64b50ea0094b8794a3a8d96db12f86&per_page=all";
 		$json_string_leg = file_get_contents($start_url_leg);
+
+	$start_url_leg_house = "http://congress.api.sunlightfoundation.com/legislators?chamber=house&apikey=dc64b50ea0094b8794a3a8d96db12f86&per_page=all";
+		$json_string_leg_house = file_get_contents($start_url_leg_house);
+
+	$start_url_leg_senate = "http://congress.api.sunlightfoundation.com/legislators?chamber=senate&apikey=dc64b50ea0094b8794a3a8d96db12f86&per_page=all";
+		$json_string_leg_senate = file_get_contents($start_url_leg_senate);
 	
 	$start_url_bill = "https://congress.api.sunlightfoundation.com/bills?apikey=dc64b50ea0094b8794a3a8d96db12f86&per_page=50";
 		$json_string_bill = file_get_contents($start_url_bill);
@@ -29,7 +35,7 @@ else{
 	$start_url_committee = "https://congress.api.sunlightfoundation.com/committees?chamber=joint&apikey=dc64b50ea0094b8794a3a8d96db12f86&per_page=all";
 		$json_string_committee_j = file_get_contents($start_url_committee);
 
-	$json_string = array($json_string_leg, $json_string_bill, $json_string_committee_h, $json_string_committee_s, $json_string_committee_j);
+	$json_string = array($json_string_leg, $json_string_leg_house, $json_string_leg_senate, $json_string_bill, $json_string_committee_h, $json_string_committee_s, $json_string_committee_j);
 	echo json_encode($json_string);
 
 	function Bill(){
